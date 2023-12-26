41c924fb77ccde4101db68cf66d426553b805002
keoni <kimkeonhwi991231@gmail.com>
2023-12-26 15:00:32 +0900

    first commit

---
+++ src/main/java/spring_mvc_http/spring_mvc_http/SpringMvcHttpApplication.java
@@ -0,0 +1,13 @@
+package spring_mvc_http.spring_mvc_http;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcHttpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcHttpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/spring_mvc_http/spring_mvc_http/SpringMvcHttpApplicationTests.java
@@ -0,0 +1,13 @@
+package spring_mvc_http.spring_mvc_http;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcHttpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
